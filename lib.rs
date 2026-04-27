63ee89b465e2f3a5f0f50eef034349f36ab5bd79
marlemjordan1982-wq <marlemjordan1982@gmail.com>
2026-04-27 11:41:41 -0600

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -10,24 +10,27 @@ pub mod mi_contador {
     pub fn crear_contador(ctx: Context<Initialize>) -> Result<()> {
         let contador = &mut ctx.accounts.contador;
         contador.valor = 0;
+        msg!("Contador creado con éxito");
         Ok(())
     }
 
    
+    pub fn leer_contador(ctx: Context<Read>) -> Result<u64> {
+        let valor_actual = ctx.accounts.contador.valor;
+        msg!("El valor actual del contador es: {}", valor_actual);
+        Ok(valor_actual)
+    }
+
+    
     pub fn incrementar(ctx: Context<Update>) -> Result<()> {
         let contador = &mut ctx.accounts.contador;
         contador.valor += 1;
         Ok(())
     }
 
-   
-    pub fn reiniciar(ctx: Context<Update>) -> Result<()> {
-        let contador = &mut ctx.accounts.contador;
-        contador.valor = 0;
-        Ok(())
-    }
-
+    
     pub fn eliminar_contador(_ctx: Context<Close>) -> Result<()> {
+        msg!("Cuenta cerrada y renta devuelta al usuario");
         Ok(())
     }
 }
@@ -42,6 +45,11 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
+pub struct Read<'info> {
+    pub contador: Account<'info, Contador>,
+}
+
+#[derive(Accounts)]
 pub struct Update<'info> {
     #[account(mut)]
     pub contador: Account<'info, Contador>,
@@ -49,7 +57,7 @@ pub struct Update<'info> {
 
 #[derive(Accounts)]
 pub struct Close<'info> {
-    #[account(mut, close = user)] // Aquí se define la eliminación
+    #[account(mut, close = user)]
     pub contador: Account<'info, Contador>,
     #[account(mut)]
     pub user: Signer<'info>,
